0d036842fcc065e2fdfe5cb257340b423fb4fd35
nil1902 <119749168+nil1902@users.noreply.github.com>
2025-07-12 09:31:01 +0530

    Initial commit to main branch

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -61,7 +61,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       setCurrentUser(user);
       setLoading(false);
     });
-
+// 
     return unsubscribe;
   }, []);
 
