fa1ab490e947065c9440d2646b65de9136be7b7e
nil1902 <nilimeshpal15#gmail.com>
2025-09-05 11:15:52 +0530

    m

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ if (process.env.TEMPO === "true") {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/nil-s-kitchen/", // Correct for GitHub Pages
+  base: "/", // Use "/" for Vercel hosting
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
     include: [
